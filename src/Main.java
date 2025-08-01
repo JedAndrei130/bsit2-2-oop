b92d17ea78028828020a72c427e2e7338bbc4315
jed andrei surabasquez <jasurabasquez66130@liceo.edu.ph>
2025-08-01 09:47:46 +0800

    lab activity 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(Striner(System.in);
+        int totalNumber = 0;g[] args) {
+            Scanner input = new Scann
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter number " + i + ": ");
+            int number = input.nextInt();
+            totalNumber += number;
+        }
+
+        System.out.println("\nTotal Number: " + totalNumber);
+        input.close();
     }
 }
