13467b8c8e74183219282867de0d4d5b3de7d79b
jed andrei surabasquez <jasurabasquez66130@liceo.edu.ph>
2025-08-04 11:45:23 +0800

    activity3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        book book1 = new book("Tristan", "George Orwell", 328);
-        book book2 = new book("To Kill a Mockingbird", "Harper Lee", 281);
-        book book3 = new book("The Great Gatsby", "F. Scott Fitzgerald", 180);
+        book book1 = new book("Tristan the MightyMan", "Tristan Amparo", 328);
+        book book2 = new book("Bob the Anjo Slayer", "Anjo Tommy", 281);
+        book book3 = new book("Anjo the Dark Knight", "Jed Surabasquez", 180);
 
         book1.displayInfo();
         book2.displayInfo();
