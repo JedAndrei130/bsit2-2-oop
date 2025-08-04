bd2838071c9a61b5b32da40da6b467255173a8d4
jed andrei surabasquez <jasurabasquez66130@liceo.edu.ph>
2025-08-04 11:27:22 +0800

    activity 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        book book1 = new book("Tristan", "George Orwell", 328);
+        book book2 = new book("To Kill a Mockingbird", "Harper Lee", 281);
+        book book3 = new book("The Great Gatsby", "F. Scott Fitzgerald", 180);
+
+        book1.displayInfo();
+        book2.displayInfo();
+        book3.displayInfo();
+
+        book1.borrowBook();
+        book1.displayInfo();
+
+        book1.returnBook();
+        book1.displayInfo();
     }
 }
