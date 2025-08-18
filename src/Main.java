4dee4b834c0a7b57286dc2a97bc415354c3281f0
jed andrei surabasquez <jasurabasquez66130@liceo.edu.ph>
2025-08-18 11:33:42 +0800

    lab-activity-4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GradeCalculator gc = new GradeCalculator();
+
+        String studentName = "John Smith";
+        double average = gc.calculateAverage(85.5, 92.0, 78.5, 90.0);
+        String letterGrade = gc.getLetterGrade(average);
+
+        gc.displayResult(studentName, average);
+        gc.displayResult(studentName, average, letterGrade);
     }
 }
